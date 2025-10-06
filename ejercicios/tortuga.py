ae26f7f21ab0c1952711a378b7d893a4c2fc60b7
pablo <pablovicentin84@gmail.com>
2025-10-06 19:52:06 -0300

    square

--- ejercicios/tortuga.py
+++ ejercicios/tortuga.py
@@ -4,8 +4,12 @@ window = turtle.Screen()
 window.bgcolor("white")
 tortuga = turtle.Turtle()
 tortuga.color("red")
-# tortuga.shape("turtle")
+tortuga.shape("turtle")
 tortuga.speed(1)
-# tortuga.forward(100)
-# tortuga.right(90)
-# tortuga.forward(100)
+
+# Dibujamos un cuadrado
+for _ in range(4):
+    tortuga.forward(100)  # Avanza 100 unidades
+    tortuga.right(90)    # Gira 90 grados a la derecha
+
+window.mainloop()  # Mantiene la ventana abierta
